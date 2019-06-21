4f933d27fbd088eb57573e263f8d9f0c6cb97424
freeza <freeza@gmail.com>
2019-06-21 09:55:32 -0700

    Increase zram devices to 4
    Signed-off-by: freeza <freeza@gmail.com>
    
    # Conflicts:
    #	drivers/block/zram/zram_drv.c

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -51,7 +51,7 @@ static int zram_major;
 static const char *default_compressor = "lzo-rle";
 
 /* Module params (documentation at end) */
-static unsigned int num_devices = 1;
+static unsigned int num_devices = 4;
 /*
  * Pages that compress to sizes equals or greater than this are stored
  * uncompressed in memory.
