61984123d617f20af6f4765ad4686fe0381e8186
freeza <freeza@gmail.com>
2019-06-20 21:31:41 -0700

    remove rodata as it isn't used.
    Signed-off-by: freeza <freeza@gmail.com>
    
    # Conflicts:
    #	arch/arm64/mm/mmu.c

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -659,7 +659,9 @@ early_param("rodata", parse_rodata);
 #ifdef CONFIG_UNMAP_KERNEL_AT_EL0
 static int __init map_entry_trampoline(void)
 {
-	pgprot_t prot = rodata_enabled ? PAGE_KERNEL_ROX : PAGE_KERNEL_EXEC;
+	/* 'DEBUG_RODATA' rodata is not enabled by default from SAMSUNG */
+	//pgprot_t prot = rodata_enabled ? PAGE_KERNEL_ROX : PAGE_KERNEL_EXEC;
+	pgprot_t prot = PAGE_KERNEL_EXEC;
 	phys_addr_t pa_start = __pa_symbol(__entry_tramp_text_start);
 
 	/* The trampoline is always mapped and can therefore be global */
