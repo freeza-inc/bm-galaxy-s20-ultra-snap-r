15f1177e10553d5a821d4c38037dd6454feb3feb
freeza <freeza@gmail.com>
2020-02-09 12:18:05 -0800

    fix compile with CONFIG_F2FS_SEC_BLOCK_OPERATIONS_DEBUG disabled
    Signed-off-by: freeza <freeza@gmail.com>

--- fs/f2fs/checkpoint.c
+++ fs/f2fs/checkpoint.c
@@ -1255,7 +1255,9 @@ retry_flush_dents:
 		if (err)
 			goto out;
 		blk_flush_plug(current);
+#ifdef CONFIG_F2FS_SEC_BLOCK_OPERATIONS_DEBUG
 		sec_dbg_add_time(dbg_entry, DENTS, s_jiffies);
+#endif
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1274,7 +1276,9 @@ retry_flush_dents:
 		if (err)
 			goto out;
 		blk_flush_plug(current);
+#ifdef CONFIG_F2FS_SEC_BLOCK_OPERATIONS_DEBUG
 		sec_dbg_add_time(dbg_entry, IMETA, s_jiffies);
+#endif
 		cond_resched();
 		goto retry_flush_quotas;
 	}
@@ -1295,7 +1299,9 @@ retry_flush_nodes:
 			goto out;
 		}
 		blk_flush_plug(current);
+#ifdef CONFIG_F2FS_SEC_BLOCK_OPERATIONS_DEBUG
 		sec_dbg_add_time(dbg_entry, NODES, s_jiffies);
+#endif
 		cond_resched();
 		goto retry_flush_nodes;
 	}
