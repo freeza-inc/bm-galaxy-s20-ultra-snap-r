a3f45d4ca9dc5a4eb9a4ef74adcf8c08bb69d912
Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
2020-04-20 11:50:23 +0530

    mmc: sdhci-msm: Enable host capabilities pertains to R1b response
    commit 9d8cb58691f85cef687512262acb2c7109ee4868 upstream.
    
    MSM sd host controller is capable of HW busy detection of device busy
    signaling over DAT0 line. And it requires the R1B response for commands
    that have this response associated with them.
    
    So set the below two host capabilities for qcom SDHC.
     - MMC_CAP_WAIT_WHILE_BUSY
     - MMC_CAP_NEED_RSP_BUSY
    
    Recent development of the mmc core in regards to this, revealed this as
    being a potential bug, hence the stable tag.
    
    Cc: <stable@vger.kernel.org> # v4.19+
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Link: https://lore.kernel.org/r/1587363626-20413-2-git-send-email-vbadigan@codeaurora.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    # Conflicts:
    #	drivers/mmc/host/sdhci-msm.c

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -6127,6 +6127,7 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 		goto vreg_deinit;
 	}
 
+
 	/*
 	 * To avoid polling and to avoid this R1b command conversion
 	 * to R1 command if the requested busy timeout > host's max
@@ -6136,6 +6137,7 @@ static int sdhci_msm_probe(struct platform_device *pdev)
 
 	msm_host->pltfm_init_done = true;
 
+	msm_host->mmc->caps |= MMC_CAP_WAIT_WHILE_BUSY | MMC_CAP_NEED_RSP_BUSY;
 	pm_runtime_set_active(&pdev->dev);
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_set_autosuspend_delay(&pdev->dev, MSM_AUTOSUSPEND_DELAY_MS);
