f5f32db8d51c817c518715ac40e5d6dd6c5117ac
freeza <freeza@gmail.com>
2020-12-23 13:37:59 -0800

    Revert "mm/vmalloc.c: don't dereference possible NULL pointer in __vunmap()"
    This reverts commit 2ade6fbda80466c5ea1be3532a766c9b6b2d2caf.

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2216,7 +2216,7 @@ static void __vunmap(const void *addr, int deallocate_pages)
 			addr))
 		return;
 
-	va = find_vm_area(addr)->vm;
+	va = find_vmap_area((unsigned long)addr);
 	if (unlikely(!va || !(va->flags & VM_VM_AREA))) {
 		WARN(1, KERN_ERR "Trying to vfree() nonexistent vm area (%p)\n",
 				addr);
